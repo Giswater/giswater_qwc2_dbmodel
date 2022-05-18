b0afff5c69fcda1f5238a24fe9dbab990f2d6489
Edgar Fuste <efuste@bgeo.es>
2022-05-18 08:48:43 +0200

    Enhancement: Update functions

--- corporate/web/ws/fct/gw_fct_getmincut_js.sql
+++ corporate/web/ws/fct/gw_fct_getmincut_js.sql
@@ -1,28 +1,60 @@
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME"."gw_fct_getmincut"(p_data json) RETURNS pg_catalog.json AS $BODY$
+-- Function: SCHEMA_NAME.gw_fct_getmincut(json)
+
+-- DROP FUNCTION SCHEMA_NAME.gw_fct_getmincut(json);
+
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_getmincut(p_data json)
+  RETURNS json AS
+$BODY$
 
 /*
+-- Button networkMincut on mincut dialog
+SELECT gw_fct_getmincut('{"data":{"mincutId":"3"}}');
+
+fid = 216
+
+WEB
+
 SELECT gw_fct_getmincut($${
 "client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, 
 "feature":{}, 
 "data":{"x":"419534.15219828", "y":"4576491.4086902", "srid_arg":"25831", "mincut_id_arg":null, "device":3, "insert_data":"arc", 
 "element_type":{}, "lang":"es"}}$$);
+
+
 */
+
 DECLARE
-    v_x double precision;
-    v_y double precision;
-    v_srid_arg integer;
-	v_mincut_id_arg integer;
-    v_device integer;
-    v_element_type character varying;
-    v_lang character varying;
-	v_user_name text;
+
+v_mincut integer;
+v_version text;
+v_error_context text;
+v_mincutrec record;
+v_result json;
+v_result_info json;
+
+v_x double precision;
+v_y double precision;
+v_srid_arg integer;
+v_mincut_id_arg integer;
+v_device integer;
+v_element_type character varying;
+v_lang character varying;
+v_user_name text;
+
 
 BEGIN
 
---  Set search path to local schema
-    SET search_path = "SCHEMA_NAME", public;
+	-- Search path
+	SET search_path = "SCHEMA_NAME", public;
 	
-	-- getting input data 
+	SELECT giswater INTO v_version FROM sys_version order by id desc limit 1;
+
+	-- delete previous
+	DELETE FROM audit_check_data WHERE fid = 216 and cur_user=current_user;
+
+	-- get input parameters
+	v_mincut := ((p_data ->>'data')::json->>'mincutId')::integer;	
+	
 	v_mincut_id_arg :=  ((p_data ->>'data')::json->>'mincut_id_arg');
 	v_x :=  ((p_data ->>'data')::json->>'x');
 	v_y :=  ((p_data ->>'data')::json->>'y');
@@ -37,14 +69,44 @@ BEGIN
 		--  Return
    		RETURN  gw_fct_getmincut(v_x, v_y, v_srid_arg, v_mincut_id_arg, v_device, v_element_type, v_lang, v_user_name);
 	END IF;
+	
+	-- mincut details
+	SELECT * INTO v_mincutrec FROM om_mincut WHERE id = v_mincut;
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, '');
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, 'Mincut stats');
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, '-----------------');
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, concat('Number of arcs: ', (v_mincutrec.output->>'arcs')::json->>'number'));
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, concat('Length of affected network: ', (v_mincutrec.output->>'arcs')::json->>'length', ' mts'));
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, concat('Total water volume: ', (v_mincutrec.output->>'arcs')::json->>'volume', ' m3'));
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, concat('Number of connecs affected: ', (v_mincutrec.output->>'connecs')::json->>'number'));
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, concat('Total of hydrometers affected: ', ((v_mincutrec.output->>'connecs')::json->>'hydrometers')::json->>'total'));
+	INSERT INTO audit_check_data (fid, error_message) VALUES (216, concat('Hydrometers classification: ', ((v_mincutrec.output->>'connecs')::json->>'hydrometers')::json->>'classified'));
 
---  Exception handling
-   /* 
-   EXCEPTION WHEN OTHERS THEN 
-        RETURN ('{"status":"Failed","SQLERR":' || to_json(SQLERRM) || ', "apiVersion":'|| v_version ||',"SQLSTATE":' || to_json(SQLSTATE) || '}')::json;
-*/
+	-- info
+	v_result = null;
+	SELECT array_to_json(array_agg(row_to_json(row))) INTO v_result 
+	FROM (SELECT id, error_message as message FROM audit_check_data WHERE cur_user="current_user"() AND fid=216 order by id) row;
+	v_result := COALESCE(v_result, '{}'); 
+	v_result_info = concat ('{"geometryType":"", "values":',v_result, '}');
 
+	v_result_info := COALESCE(v_result_info, '{}'); 
+
+	-- return
+	RETURN ('{"status":"Accepted", "version":"'||v_version||'","body":{"form":{}'||
+			',"data":{ "info":'||v_result_info||'}'||
+			'}}')::json;
+
+	--  Exception handling
+	EXCEPTION WHEN OTHERS THEN
+	GET STACKED DIAGNOSTICS v_error_context = pg_exception_context;  
+	RETURN ('{"status":"Failed", "SQLERR":' || to_json(SQLERRM) || ',"SQLCONTEXT":' || to_json(v_error_context) || ',"SQLSTATE":' || to_json(SQLSTATE) || '}')::json;
+	
 END;
 $BODY$
-LANGUAGE 'plpgsql' VOLATILE COST 100;
-
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION SCHEMA_NAME.gw_fct_getmincut(json)
+  OWNER TO role_admin;
+GRANT EXECUTE ON FUNCTION SCHEMA_NAME.gw_fct_getmincut(json) TO public;
+GRANT EXECUTE ON FUNCTION SCHEMA_NAME.gw_fct_getmincut(json) TO role_admin;
+GRANT EXECUTE ON FUNCTION SCHEMA_NAME.gw_fct_getmincut(json) TO role_basic;
--- corporate/web/ws/fct/gw_fct_setmincut.sql
+++ corporate/web/ws/fct/gw_fct_setmincut.sql
@@ -1,6 +1,34 @@
-CREATE OR REPLACE FUNCTION "SCHEMA_NAME"."gw_fct_setmincut"(p_data json) RETURNS pg_catalog.json AS $BODY$
+-- Function: SCHEMA_NAME.gw_fct_setmincut(json)
+
+-- DROP FUNCTION SCHEMA_NAME.gw_fct_setmincut(json);
+
+CREATE OR REPLACE FUNCTION SCHEMA_NAME.gw_fct_setmincut(p_data json)
+  RETURNS json AS
+$BODY$
 
 /*
+-- Button networkMincut on mincut dialog
+SELECT gw_fct_setmincut('{"data":{"action":"mincutNetwork", "arcId":"2001", "mincutId":"3", "usePsectors":false}}');
+
+-- Button valveUnaccess on mincut dialog
+SELECT gw_fct_setmincut('{"data":{"action":"mincutValveUnaccess", "nodeId":1001, "mincutId":"3", "usePsectors":false}}');
+
+-- Button Accept on mincut dialog
+SELECT gw_fct_setmincut('{"data":{"action":"mincutAccept", "mincutClass":1, "mincutId":"3", "status":"check", "usePsectors":false}}');
+
+-- Button Accept on mincut conflict dialog
+SELECT gw_fct_setmincut('{"data":{"action":"mincutAccept", "mincutClass":1, "mincutId":"3", "status":"continue"}}');
+
+-- Button Accept when is mincutClass = 2
+SELECT gw_fct_setmincut('{"data":{"action":"mincutAccept", "mincutClass":2, "mincutId":"3"}}');
+
+-- Button Accept when is mincutClass = 3
+SELECT gw_fct_setmincut('{"data":{"action":"mincutAccept", "mincutClass":3, "mincutId":"3"}}');
+
+fid = 216
+
+WEB
+
 SELECT gw_fct_setmincut($${
 "client":{"device":4, "infoType":1, "lang":"ES"}, "form":{}, 
 "feature":{}, 
@@ -8,25 +36,48 @@ SELECT gw_fct_setmincut($${
 "insert_data":{"mincut_state":"0","mincut_type":"Real","anl_cause":"1","assigned_to":"abofill","anl_descript":"test","anl_tstamp":"2022-05-13 10:32:45",
 "forecast_start":null,"forecast_end":null,"muni_id":"1","postcode":"08830","streetaxis_id":"1-11000C","exec_start":null,"exec_appropiate":"false",
 "exec_end":null}, "element_type":"arc", "id_arg":"2095"}}$$);  
-*/
-DECLARE
-    v_mincut_id_arg integer;
-    v_x double precision;
-    v_y double precision;
-    v_srid_arg integer;
-    v_device integer;
-    v_insert_data json;
-    v_element_type character varying;
-    v_id_arg character varying;
-	v_user_name text;
-	v_prev_user_name text;
 
+*/
+
+DECLARE
+
+v_arc integer;
+v_id integer;
+v_node integer;
+v_mincut integer;
+v_status boolean;
+v_valveunaccess json;
+v_action text;
+v_mincut_class integer;
+v_version text;
+v_error_context text;
+v_usepsectors boolean;
+
+v_mincut_id_arg integer;
+v_x double precision;
+v_y double precision;
+v_srid_arg integer;
+v_device integer;
+v_insert_data json;
+v_element_type character varying;
+v_id_arg character varying;
+v_user_name text;
+v_prev_user_name text;
+	
 BEGIN
 
---  Set search path to local schema
-    SET search_path = "SCHEMA_NAME", public;
+	-- Search path
+	SET search_path = "SCHEMA_NAME", public;
+	SELECT giswater INTO v_version FROM sys_version order by id desc limit 1;
+
+	-- get input parameters
+	v_action := (p_data ->>'data')::json->>'action';
+	v_mincut := ((p_data ->>'data')::json->>'mincutId')::integer;
+	v_mincut_class := ((p_data ->>'data')::json->>'mincutClass')::integer;
+	v_node := ((p_data ->>'data')::json->>'nodeId')::integer;
+	v_arc := ((p_data ->>'data')::json->>'arcId')::integer;
+	v_usepsectors := ((p_data ->>'data')::json->>'usePsectors')::boolean;
 	
-	-- getting input data 
 	v_mincut_id_arg :=  ((p_data ->>'data')::json->>'mincut_id_arg');
 	v_x :=  ((p_data ->>'data')::json->>'x');
 	v_y :=  ((p_data ->>'data')::json->>'y');
@@ -38,23 +89,48 @@ BEGIN
 	v_user_name := (p_data ->> 'client')::json->> 'user_name';
 	
 	IF v_device IN (1,2,3) THEN
-		v_prev_user_name = current_user;
-		v_user_name := (p_data ->> 'client')::json->> 'user_name';
-		EXECUTE 'SET ROLE ' || v_user_name || '';
 		--  Return
    		RETURN  gw_fct_upsertmincut(v_mincut_id_arg, v_x, v_y, v_srid_arg, v_device, v_insert_data, v_element_type, v_id_arg, v_user_name);
-		
-		EXECUTE 'SET ROLE ' || v_prev_user_name || '';
-		raise notice 'FINISH -> %',current_user;
 	END IF;
+	
+	-- delete previous
+	DELETE FROM audit_check_data WHERE fid = 216 and cur_user=current_user;
+	
+	IF v_action = 'mincutNetwork' THEN
 
---  Exception handling
-/*
-    EXCEPTION WHEN OTHERS THEN 
-        RETURN ('{"status":"Failed","SQLERR":' || to_json(SQLERRM) || ', "apiVersion":'|| v_version ||',"SQLSTATE":' || to_json(SQLSTATE) || '}')::json;
-*/
+		RETURN gw_fct_mincut(v_arc::text, 'arc'::text, v_mincut, v_usepsectors);
+		
+	ELSIF v_action = 'mincutValveUnaccess' THEN
 
+		RETURN gw_fct_json_create_return(gw_fct_mincut_valve_unaccess(p_data), 2980, null, null, null);
+		
+	ELSIF v_action = 'mincutAccept' THEN
+
+		IF v_mincut_class = 1 THEN
+
+			UPDATE config_param_user SET value = v_mincut::text WHERE parameter = 'inp_options_valve_mode_mincut_result' AND cur_user = current_user;
+		
+			RETURN gw_fct_mincut_result_overlap(p_data);
+
+		ELSIF v_mincut_class IN (2, 3) THEN
+		
+			RETURN gw_fct_mincut_connec(p_data);
+		
+		END IF;
+
+	END IF;
+	
+	--  Exception handling
+	EXCEPTION WHEN OTHERS THEN
+	GET STACKED DIAGNOSTICS v_error_context = pg_exception_context;  
+	RETURN ('{"status":"Failed", "SQLERR":' || to_json(SQLERRM) || ',"SQLCONTEXT":' || to_json(v_error_context) || ',"SQLSTATE":' || to_json(SQLSTATE) || '}')::json;
+	
 END;
 $BODY$
-LANGUAGE 'plpgsql' VOLATILE COST 100;
-
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION SCHEMA_NAME.gw_fct_setmincut(json)
+  OWNER TO role_admin;
+GRANT EXECUTE ON FUNCTION SCHEMA_NAME.gw_fct_setmincut(json) TO public;
+GRANT EXECUTE ON FUNCTION SCHEMA_NAME.gw_fct_setmincut(json) TO role_admin;
+GRANT EXECUTE ON FUNCTION SCHEMA_NAME.gw_fct_setmincut(json) TO role_basic;
